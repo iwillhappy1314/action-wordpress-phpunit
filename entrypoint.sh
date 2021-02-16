9ed9c7a6c2971c38aaf486c27a5b440dbc38e5da
Amos Lee <iwillhappy1314@gmail.com>
2021-02-16 20:39:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,6 @@
 # it does not exit with a 0, and I only care about the final exit.
 set -eo
 
-systemctl start mysql.service
 chmod +x bin/install-wp-tests.sh
 bin/install-wp-tests.sh wordpress_test root 'root' localhost $WP_VERSION
 composer global require "phpunit/phpunit=5.7.*"
